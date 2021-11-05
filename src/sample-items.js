ed0f9583579398114633352225807b8fd429b5b0
aman-maharshi <amanm4harshi@gmail.com>
2021-11-05 12:59:42 +0530

    fonts removed

--- src/sample-items.js
+++ src/sample-items.js
@@ -1,7 +1,7 @@
 module.exports = {
     item1: {
         name: "Light Olive Shirt",
-        image: "/images/one.png",
+        image: "./images/one.png",
         desc: "Olive green solid casual shirt, has a spread collar, long sleeves, curved hem, one patch pocket",
         price: 1500,
         status: "available"
@@ -9,7 +9,7 @@ module.exports = {
 
     item2: {
         name: "Maroon Stripe Shirt",
-        image: "/images/two.png",
+        image: "./images/two.png",
         desc: "Maroon and White striped casual shirt, has a spread collar, long sleeves, button placket, curved hem.",
         price: 1700,
         status: "available"
@@ -17,7 +17,7 @@ module.exports = {
 
     item3: {
         name: "Brown Stripe Shirt",
-        image: "/images/three.png",
+        image: "./images/three.png",
         desc: "Brown and white vertical stripes striped opaque casual shirt ,has a button-down collar, button placket",
         price: 1400,
         status: "unavailable"
@@ -25,7 +25,7 @@ module.exports = {
 
     item4: {
         name: "Blended Stipes Shirt",
-        image: "/images/four.png",
+        image: "./images/four.png",
         desc: "Tri color opaque Casual shirt ,has a spread collar, button placket, long roll-up sleeves, curved hem.",
         price: 1500,
         status: "available"
@@ -33,7 +33,7 @@ module.exports = {
 
     item5: {
         name: "Black Hoodie",
-        image: "/images/five.png",
+        image: "./images/five.png",
         desc: "Black, grey and white brand logo print sweatshirt.",
         price: 2500,
         status: "available"
